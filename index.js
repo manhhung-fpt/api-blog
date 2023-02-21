1addb1698db37fd801d879f1ec78093d80b83fbc
MSI\ADMIN <hungtmse150230@fpt.edu.vn>
2023-02-21 22:19:30 +0700

    update

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
